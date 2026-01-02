2532d8202ba1e6fad7f2b38a47f285569522f46a
0318lyj <0318lyj@gmail.com>
2026-01-02 23:33:42 +0900

    first commit

---
+++ src/main/java/com/politicslive/world_politics_live/WorldPoliticsLiveApplication.java
@@ -0,0 +1,13 @@
+package com.politicslive.world_politics_live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldPoliticsLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldPoliticsLiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/politicslive/world_politics_live/WorldPoliticsLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.politicslive.world_politics_live;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldPoliticsLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
